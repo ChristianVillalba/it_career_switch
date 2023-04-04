47fc73dd5a9b8349b4e8e4f33418821e17676478
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-04-04 09:41:31 +0100

    adds more classes lessons

---
+++ Python/Classes/instance_varialbes.py
@@ -0,0 +1,14 @@
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
+
+print(alternative_rocks.store_name)
+print(alternative_rocks)
+print("")
+print(isabelles_ices.store_name)
+print(isabelles_ices)
