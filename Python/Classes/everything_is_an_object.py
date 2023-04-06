760d202585eb103500688738afcab1778b72304f
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-04-06 12:08:21 +0100

    adds more class lessons to python file

---
+++ Python/Classes/everything_is_an_object.py
@@ -0,0 +1,9 @@
+# dir() is short for directory and offers an organized presentation of object attributes.
+# example number int 5
+print(dir(5))
+
+def this_function_is_an_object():
+  return("yes")
+print("")
+# example, the function I just created
+print(dir(this_function_is_an_object))
