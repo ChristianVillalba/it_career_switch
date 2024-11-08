5c02d2d420fa114b943c06bf1f58fb49bc092e39
Christian Villalba UK <christianvillalba.uk@gmail.com>
2024-11-08 23:59:17 +0000

    ready to back end 2nd api

---
+++ PHP/geoNamesExercise/libs/php/getEarthquakesInfo.php
@@ -0,0 +1,35 @@
+<!-- <?php
+	// remove for production
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+	// Concatenates the url for the API call
+	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
+
+	// Initiates the cURL object and sets some parameters
+	// Curl: It is a tool for transferring files and data with URL syntax
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	// Executes the cURL object and stores the results to $result.
+	$result=curl_exec($ch);
+	curl_close($ch);
+
+	// This1245 API returns data as JSON and so we decode it as an associative array
+	$decode = json_decode($result,true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode['geonames'];
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output);  -->
+
+?>
